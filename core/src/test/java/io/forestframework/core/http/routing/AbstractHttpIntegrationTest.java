aad81cd29322f81fd3ee94483d99a4e1732acd1f
Lisa <liyu.italy@hotmail.com>
2020-09-10 12:10:21 +0800

    Test

--- core/src/test/java/io/forestframework/core/http/routing/AbstractHttpIntegrationTest.java
+++ core/src/test/java/io/forestframework/core/http/routing/AbstractHttpIntegrationTest.java
@@ -1,9 +1,11 @@
 package io.forestframework.core.http.routing;
 
 import io.forestframework.core.config.Config;
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Assertions;
@@ -21,7 +23,14 @@ public class AbstractHttpIntegrationTest {
 
     public HttpResponse sendHttpRequest(String method, String path) throws IOException {
         String uri = "http://localhost:" + port + path;
-        HttpUriRequest request = RequestBuilder.create(method).setUri(uri).build();
+        HttpUriRequest request = RequestBuilder
+                .create(method)
+                .setUri(uri)
+                .setHeader("Accept", String.valueOf(ContentType.APPLICATION_JSON))
+                .setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+//                .setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+//                .setHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+                .build();
         CloseableHttpResponse response = client.execute(request);
         return new HttpResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
     }
@@ -41,6 +50,14 @@ public class AbstractHttpIntegrationTest {
 
         HttpResponse assert404() {
             return assertStatusCode(404);
+        }
+
+        HttpResponse assert405() {
+            return assertStatusCode(405);
+        }
+
+        HttpResponse assert406() {
+            return assertStatusCode(406);
         }
 
         HttpResponse assert500() {
